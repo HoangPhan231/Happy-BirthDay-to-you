f5d9394a90dfeb5b89d979fc750c9c6fed7b799c
HoangPhan231 <phanthehoang2k3@gmail.com>
2023-05-22 14:28:10 +0700

    upload source code

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em Diễm Hằng';
+const giftUrl = 'https://hoangphan231.github.io/Rabit/';
+const eventName = 'Mừng sinh nhật';
+const titleCard = 'Tặng em';
+const contentCard = 
+'Dù cho kết quả có như thế nào. Ngày sinh nhật này, hãy ăn mừng và tận hưởng những kỷ niệm đáng nhớ, cùng với niềm tự hào về sự cống hiến và nỗ lực đã đem lại thành quả trong kỳ thi. Em xứng đáng nhận được tất cả những điều tốt đẹp nhất trên hành trình của mình. Chúc em có một ngày sinh nhật thật đáng nhớ!';
+const giftImage = 'DH1.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
